687240d5e1e681ee613262a8942fcac001292aa0
Siya Verma <siyaverma137@gmail.com>
2024-07-10 14:07:50 +0530

    Commit for a Blog App using Appwrite as backend as a service

---
+++ BlogApp/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      <img src="https://images.pexels.com/photos/2230796/pexels-photo-2230796.jpeg?auto=compress&cs=tinysrgb&w=400" alt="logo" width="70px" height="70px"/>
+    </div>
+  )
+}
+
+export default Logo
